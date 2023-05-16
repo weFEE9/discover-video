beb0e1affda3f42c55530916035882c809e1b8f5
weeee9 <teletubby332@gmail.com>
2023-05-16 22:07:06 +0800

    feat: add a new component and images for Home page
    - Add a new component `Banner` to the project
    - Update the `Home` component to use `Banner`
    - Add CSS styles for `Banner` in a new file `banner.module.css`
    - Add two new image files: `clifford.webp` and `play_arrow.svg`
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 
+import Banner from '@/components/banner/banner';
+
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>Netflix</main>
+      <main className={styles.main}>
+        <h1>Netflix</h1>
+
+        {/* <NavBar /> */}
+        <Banner
+          title={'Clifford the red dog'}
+          subTitle={'a very cute dog'}
+          imgUrl={'/static/clifford.webp'}
+        />
+
+        {/* <Card/> */}
+      </main>
     </div>
   );
 }
---
+++ components/banner/banner.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import Image from 'next/image';
+
+import styles from './banner.module.css';
+
+type props = {
+  title: string;
+  subTitle: string;
+  imgUrl: string;
+};
+
+const Banner = ({ title, subTitle, imgUrl }: props) => {
+  const handleOnPlay = () => {
+    console.log('play');
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+
+          <h2 className={styles.title}>{title}</h2>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+
+          <div className={styles.playBtnWrapper}>
+            <button
+              className={styles.btnWithIcon}
+              type='button'
+              onClick={handleOnPlay}
+            >
+              <Image
+                src={'/static/play_arrow.svg'}
+                alt={'play icon'}
+                width={'32'}
+                height={'32'}
+              />
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div
+        className={styles.bannerImg}
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          backgroundSize: 'cover',
+          backgroundPosition: '50% 50%',
+        }}
+      >
+        {' '}
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
