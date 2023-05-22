98ccfd84f3f281c4b16a418152afe7edb6360588
weeee9 <teletubby332@gmail.com>
2023-05-22 21:25:23 +0800

    style: refactor card component layout for improved styling
    - Add a container div for the card component
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -23,17 +23,19 @@ const Card = ({ id, imgUrl, size = 'medium' }: props) => {
   const scale = id === 0 ? { scaleY: 1.1 } : { scale: 1.1 };
 
   return (
-    <motion.div
-      className={classNames(styles.imgMotionWrapper, sizeClassMap[size])}
-      whileHover={scale}
-    >
-      <Image
-        className={styles.cardImg}
-        src={imgUrl}
-        alt='picture of the author'
-        fill
-      />
-    </motion.div>
+    <div className={styles.container}>
+      <motion.div
+        className={classNames(styles.imgMotionWrapper, sizeClassMap[size])}
+        whileHover={scale}
+      >
+        <Image
+          className={styles.cardImg}
+          src={imgUrl}
+          alt='picture of the author'
+          fill
+        />
+      </motion.div>
+    </div>
   );
 };
 
