b1784b8d27378d163480146e8e5346b1a3ad9fc4
weeee9 <teletubby332@gmail.com>
2023-05-27 11:07:12 +0800

    fix: improve error handling and logging for YouTube API requests
    - Add error handling for failed YouTube API requests
    - Improve error messages with response text
    - Add console error logging for caught errors
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- lib/videos.ts
+++ lib/videos.ts
@@ -7,19 +7,29 @@ export type Video = {
 export const getVideos = async (searchQuery: string): Promise<Video[]> => {
   const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
-  const response = await fetch(
-    `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${searchQuery}&type=video&key=${YOUTUBE_API_KEY}`
-  );
+  try {
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${searchQuery}&type=video&key=${YOUTUBE_API_KEY}`
+    );
 
-  const data: YoutubeAPIResponse = await response.json();
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Failed to fetch videos from YouTube API: ${errorText}`);
+    }
 
-  return data.items.map((item) => {
-    return {
-      id: item.id.videoId,
-      title: item.snippet.title,
-      imgUrl: item.snippet.thumbnails.high.url,
-    };
-  });
+    const data: YoutubeAPIResponse = await response.json();
+
+    return data.items.map((item) => {
+      return {
+        id: item.id.videoId,
+        title: item.snippet.title,
+        imgUrl: item.snippet.thumbnails.high.url,
+      };
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 type YoutubeAPIResponse = {
